0b3c8f35cd5122114b1bc472ea6d3802c32eba04
Jevaa Kharthik N <113284998+Jevaa-kharthik@users.noreply.github.com>
2024-06-18 10:17:11 +0530

    Create crunch.sh

---
+++ crunch.sh
@@ -0,0 +1,41 @@
+#! /bin/bash
+
+echo -n "Enter Father name :"
+read father
+
+echo -n "Enter Mother name : "
+read mother
+
+echo -n "Enter Pet name : "
+read pet_name
+
+echo -n "Born City : "
+read born_city
+
+echo -n "Date of Birth YYYY-MM-DD : "
+read dob
+
+echo "The Filename Extension should end with .txt"
+echo -n "Enter Filename : "
+read filename
+
+file_extension(){
+	extension="${filename##*.}"
+}
+
+if [ $extension -e ".txt" ]; then
+	echo -n "Minimum Length to generate Password : "
+	read min
+
+	echo -n "Maximum Length to generate password : "	
+	read max
+
+	character_crunch="${father}${mother}${pet_name}${born_city}${dob}"
+
+	#using crunch tool to generate password
+
+	crunch $min $max -t "${character_crunch}@@@@@@@" -o $filename
+
+	echo "$filename is Successfully Generated..."
+
+
