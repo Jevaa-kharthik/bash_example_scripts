9179bbe56ec421a6175fcc9144cb890c9bfda3b9
Jevaa Kharthik N <113284998+Jevaa-kharthik@users.noreply.github.com>
2024-06-18 10:34:19 +0530

    Update crunch.sh

--- crunch.sh
+++ crunch.sh
@@ -1,47 +1,64 @@
-#! /bin/bash
+#!/bin/bash
 
-echo -n "Enter Father name :"
-read father
-
-echo -n "Enter Mother name : "
-read mother
-
-echo -n "Enter Pet name : "
-read pet_name
-
-echo -n "Born City : "
-read born_city
-
-echo -n "Date of Birth YYYY-MM-DD : "
-read dob
-
-echo "The Filename Extension should end with .txt"
-echo -n "Enter Filename : "
-read filename
-
-file_extension(){
-	extension="${filename##*.}"
-}
-
-file_extension
-
-if [[ $extension == "txt" ]]; then
-	echo -n "Minimum Length to generate Password : "
+# Main Function for the input
+main(){
+	echo -n "Minimum Length to generate Password: "
 	read min
 
-	echo -n "Maximum Length to generate password : "	
+	echo -n "Maximum Length to generate Password: "
 	read max
 
+	echo -n "Enter your father's name: "
+	read father
+
+	echo -n "Enter your mother's name: "
+	read mother
+
+	echo -n "Enter your pet's name: "
+	read pet_name
+
+	echo -n "Enter your birth city: "
+	read born_city
+
+	echo -n "Enter your date of birth (DDMMYYYY): "
+	read dob
+
+	# Combine all the inputs
 	character_crunch="${father}${mother}${pet_name}${born_city}${dob}"
 
-	#using crunch tool to generate password
+	# Using crunch tool to generate password
+	crunch $min $max -t "${character_crunch}@@@@@@@" -o "$filename"
 
-	crunch $min $max -t "${character_crunch}@@@@@@@" -o $filename
+	echo "$filename is successfully generated..."
+}
 
-	echo "$filename is Successfully Generated..."
-else
+# Function to get the file extension
+file_extension() {
+  extension="${filename##*.}"
+}
+
+# Read filename
+echo -n "Enter the output filename (with .txt extension): "
+read filename
+
+# Call the function to get the file extension
+file_extension
+
+if [[ -f $filename ]]; then
+	# Check the file extension
+	if [[ $extension == "txt" ]]; then
+		main
+	else
 	echo "You have given the wrong file extension"
-	exit
+	exit 1
+	fi
+else 
+	touch $filename
+	echo "Successfully Created $filename"
+	if [[ $extension == "txt" ]]; then
+		main
+	else
+		echo "You have given the wrong file extension"
+	fi
 fi
-
 
